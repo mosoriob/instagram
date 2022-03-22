ff86a968b202e1cc5a8e36b6a0921ddcd9be9836
Maximilinano Osorio <mosorio@isi.edu>
2022-03-22 09:57:50 -0300

    fix: login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,18 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
 import * as ROUTES from '../constants/routes';
 
 export default function Login() {
+    const { firebase } = useContext(FirebaseContext);
+    
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const [error, setError] = useState('');
+    const isInvalid = password === '' || emailAddress === '';
+    
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        
+        try {
+            await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
+        } catch (error) {
+            setEmailAddress('');
+            setPassword('');
+            setError(error.message);
+        }
+    }
+    
     useEffect(() => {
-        document.title = 'Login';
+        document.title = 'Login - Instagram';
     }, []);
     
-    const [email, setEmail] = React.useState('');
-    const [password, setPassword] = React.useState('');
-    const isInvalid = email === '' || password === '';
-    const [error, setError] = React.useState(null);
-    console.log('Login');
-
     return (
         <div className="container flex mx-auto max-w-screen-md items-center h-screen">
             <div className="flex w-3/5">
@@ -23,28 +38,28 @@ export default function Login() {
                 <h1 className="flex justify-center w-full">
                     <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
                 </h1>
+                {error && <p className="mb-4 text-xs text-red-500">{error}</p>}
                 
-                <form method="POST">
+                <form onSubmit={handleLogin} method="POST">
                     <input
                         aria-label="Enter your email address"
                         className="text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2"
                         type="text"
                         placeholder="Email address"
-                        onChange={({ target }) => setEmail(target.value)}
-                        name="email"
+                        onChange={({ target }) => setEmailAddress(target.value)}
                     />
                     <input
                         aria-label="Enter your password"
                         className="text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2"
                         type="password"
                         placeholder="Password"
-                        name="password"
                         onChange={({ target }) => setPassword(target.value)}
                     />
                     <button
-                        type="submit"
                         disabled={isInvalid}
-                        className={`bg-blue-500 text-white w-full rounded h-8 font-bold ${isInvalid && 'opacity-50'}`}
+                        type="submit"
+                        className={`bg-blue-500 text-white w-full rounded h-8 font-bold ${ isInvalid && 'cursor-not-allowed opacity-50'
+                        }`}
                     >
                         Log In
                     </button>
